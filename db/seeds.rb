5060e369f08e2bf469545e6af472d02d633c52b1
lalalainexd <lalalainexd@gmail.com>
2013-03-21 23:23:28 -0600

    seeding db with e-kitten

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Product.create(name:"E-Kitten", description:"E-Kitten is the only kitten you can take anywhere with you! It's hypoallergenic! Great for kids and stays a kitten! Get your E-Kitten now!", price: 50.12)
