86d73702b591ab4c1c7ca8b3a704abdc5b0933cd
lalalainexd <lalalainexd@gmail.com>
2013-03-21 21:16:08 -0600

    changed products specs to test that the actual product object is sent and used in the view

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe ProductsController do
 
+  describe 'GET #index' do
+    it "assigns product to a product" do
+      product = Product.create!(name: "sample")
+
+      products = [product]
+
+      get :index
+      expect(assigns(:products)).to match_array products
+
+    end
+  end
+
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,32 +2,33 @@ require 'spec_helper'
 
 describe 'browsing products' do
 
-  let(:product){Product.create!(name: 'name', description: 'description')}
-  it 'displays all products' do 
-    product
+  let!(:product){Product.create!(name: 'name', description: 'description')}
+
+  it 'displays all products' do
+    Product.any_instance.should_receive(:name).and_return('name')
     visit '/'
-    page.should have_content('Amazing eBook')
+    page.should have_content('name')
   end
 
   it "displays an image"
 
 
   it "displays a description" do
-    product
+    Product.any_instance.should_receive(:description).and_return('description')
     visit '/'
-    page.should have_content('somedescription')
+    page.should have_content('description')
   end
 
-  describe "buy link" do 
+  describe "buy link" do
     it "has a buy button" do
       visit '/'
       page.should have_link('Buy!', href: '/checkout')
     end
 
-    it "redirects to the checkout page from the buy button" do 
+    it "redirects to the checkout page from the buy button" do
       visit '/'
       click_link("Buy!")
       current_path.should == '/checkout'
-    end 
+    end
   end
 end
