5f53fd0278ebb59fb0db1b0ee753e07086a5de65
Jennifer Eliuk <jen7maples@gmail.com>
2013-03-25 17:41:02 -0600

    create validation tests for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price
+  validates :name, :description, :price, presence: true
 
   has_many :cart_products
   has_many :carts, through: :cart_products
