b33a8265b9556c95fdfb69902549d26c422a9ba5
Jennifer Eliuk <jen7maples@gmail.com>
2013-03-26 16:13:46 -0600

    create categoris, unfinished

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -12,9 +12,24 @@ describe "category pages" do
       end
     end
 
+    context "when information is invalid" do
+      it "doesn't create a new category" do
+        fill_in "Name", with: ""
+        expect{ click_button "Create Category" }.to_not change(Category, :count).by(1)
+      end
+    end
   end
 
+  describe "modifying a category" do
+    let!(:category){Category.create!(name: "shoes")}
+    before { visit edit_category_path }
 
+    it "modifies the category name" do
+      fill_in "Name", with: "dogs"
+      expect{ }
+    end
+
+  end
 
 
 end
