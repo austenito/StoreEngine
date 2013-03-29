0d2c0e3b757f72ffb432aaad36ba242a8d409076
lalalainexd <lalalainexd@gmail.com>
2013-03-28 21:58:40 -0600

    added images to edit, index, and show for admin products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price, :retired, :image
   validates :name, :description, :price, presence: true
-  validates_attachment :image, :content_type {content_type: "image/jpg"}
 
   has_many :cart_products
   has_many :carts, through: :cart_products
