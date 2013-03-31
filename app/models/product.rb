2c67ab892779c23b7cd16b268bdaa38161e98d3d
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-03-31 12:55:44 -0600

    changed product creation scoping and added a description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price, :retired, :image
   validates :name, :description, :price, presence: true
+  validates_uniqueness_of :name
 
   has_many :cart_products
   has_many :carts, through: :cart_products
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,36 +2,97 @@ require 'spec_helper'
 
 describe Product do
 
-  let(:product) { Product.new }
+  describe "when a product is missing attributes" do 
 
-  context "when missing a name" do
-    it "is invalid" do
-      expect(product).to be_invalid
+    let(:product) { Product.new }
+
+    context "when missing a name" do
+      it "is invalid" do
+        expect(product).to be_invalid
+      end
+
+      it "has errors" do
+        expect(product).to have(1).errors_on :name
+      end
     end
 
-    it "has errors" do
-      expect(product).to have(1).errors_on :name
+    context "when name is an empty string" do 
+      it "is invalid" do 
+        product.name = ""
+        expect(product).to be_invalid
+      end
+
+      it "has errors" do 
+        expect(product).to have(1).errors_on :name
+      end 
+    end
+
+    context "when missing a description" do
+      it "is invalid" do
+        expect(product).to be_invalid
+      end
+
+      it "has errors" do
+        expect(product).to have(1).errors_on :description
+      end
+    end
+
+    context "when missing a price" do
+      it "is invalid" do
+        expect(product).to be_invalid
+      end
+
+      it "has errors" do
+        expect(product).to have(1).errors_on :price
+      end
     end
   end
 
-  context "when missing a description" do
-    it "is invalid" do
-      expect(product).to be_invalid
+  describe "when a product has invalid attributes" do 
+
+    let(:product) { Product.new(name: "grapes", description: "good", price: 3.00) }
+
+    context "when its name is not unique" do
+      it "is invalid" do 
+        Product.create(name: "grapes", description: "sweet", price: 2.00)
+        expect(product).to be_invalid
+      end
+
+      it "has errors" do 
+      end 
+    end 
+
+    context "when description is an empty string" do 
+      it "is invalid" do 
+      end 
+
+      it "has errors" do 
+      end 
+    end 
+
+    context "when price is not a valid decimal numeric value" do 
+      it "is invalid" do 
+      end 
+
+      it "has errors" do 
+      end 
+    end 
+
+    context "when price is not greater than zero" do 
+      it "is invalid" do 
+      end 
+
+      it "has errors" do 
+      end 
     end
 
-    it "has errors" do
-      expect(product).to have(1).errors_on :description
-    end
-  end
+    context "when a photo is not a valid URL format" do 
+      it "is invalid" do 
+      end 
 
-  context "when missing a price" do
-    it "is invalid" do
-      expect(product).to be_invalid
-    end
-
-    it "has errors" do
-      expect(product).to have(1).errors_on :price
-    end
+      it "has errors" do 
+      end 
+    end 
   end
 
   context "when valid" do
@@ -40,5 +101,4 @@ describe Product do
       expect(product).to be_valid
     end
   end
-
 end
