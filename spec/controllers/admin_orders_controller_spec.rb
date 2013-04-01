cbfe9b3c61147bf73d666536722ed841d9857140
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-03-31 18:50:08 -0600

    cleaned up tests for repetition

--- spec/controllers/admin_orders_controller_spec.rb
+++ spec/controllers/admin_orders_controller_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe Admin::OrdersController do
 
-  context "an admin visits their order page" do 
+  let(:product) { Product.create(name: "banana", description: "yummy", price: 2.00) }
+  let(:order) { Order.new(quantity: 2, status: "shipped", user_id: 1) }
+    
+  before do 
+    order.products << product
+    order.save
+  end
+
+  context "an admin visits their order page" do  
 
     it "renders all orders" do 
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, user_id: 1)
-      order.products << product
-      order.save
       orders = Order.all
       get :index
       expect(assigns(:orders)).to match_array orders
     end 
 
     it "shows total number of orders by status" do 
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "shipped", user_id: 1)
-      order.products << product
-      order.save
       get :index
       expect(Order.find_all_by_status("shipped").count).to eq 1 
     end 
@@ -30,40 +30,27 @@ describe Admin::OrdersController do
     end 
 
     it "returns the products associated with an order" do
-      new_product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "shipped", user_id: 1)
-      order.products << new_product
-      order.save
       get :show, {id: 1}
-      expect(assigns(:order).products.first.name).to eq new_product.name
+      expect(assigns(:order).products.first.name).to eq product.name
     end 
   end 
 
   context "an admin clicks to update an order" do
 
     it "cancels a pending order" do
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "pending", user_id: 1)
-      order.products << product
-      order.save
+      order.status = "cancelled"
       post :cancel, { id: order.id } 
       expect(assigns(:order).status).to eq "cancelled"
     end 
 
     it "marks as returned orders that are currently marked as shipped" do
-      product = Product.create(name: "banana", description: "yummy", price: 2.00) 
-      order = Order.new(quantity: 2, status: "pending", user_id: 1)
-      order.products << product
-      order.save
+      order.status = "shipped"
       post :return, { id: order.id }
       expect(assigns(:order).status).to eq "returned"
     end 
 
     it "marks as shipped orders that are currently paid" do 
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "paid", user_id: 1)
-      order.products << product
-      order.save
+      order.status = "paid"
       post :ship, { id: order.id }
       expect(assigns(:order).status).to eq "shipped"
     end 
@@ -72,20 +59,13 @@ describe Admin::OrdersController do
   context "an admin clicks to view orders by status" do 
 
     it "filters orders by shipped status" do
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "shipped", user_id: 1) 
-      order.products << product
-      order.save
       shipped_orders = Order.find_all_by_status("shipped")
       get :index, { status: "shipped"}
       expect(assigns(:orders)).to eq shipped_orders
     end 
 
     it "filters all cancelled orders" do 
-      product = Product.create(name: "banana", description: "yummy", price: 2.00)
-      order = Order.new(quantity: 2, status: "cancelled") 
-      order.products << product
-      order.save
+      order.status = "cancelled"
       other_order = Order.new(quantity: 2, status: "shipped", user_id: 1)
       other_order.products << product
       order.save
