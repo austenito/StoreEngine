60e6c6fa819faf478c756735031455a3377519c8
lalalainexd <lalalainexd@gmail.com>
2013-04-02 17:39:52 -0600

    added test that user must be loged in to see orders page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,10 @@ class OrdersController < ApplicationController
 
   def check_authentication
     if !current_user
-      redirect_to new_user_session_path
-    else
+      redirect_to login_path
     end
   end
-  
+
   def index
     @orders = Order.find_all_by_user_id(current_user.id)
   end
