cdd0918bcb58f8ee6f80221079a11460d554473b
lalalainexd <lalalainexd@gmail.com>
2013-04-02 17:12:40 -0600

    added authorization stuff to admin categories

--- spec/controllers/admin_categories_controller_spec.rb
+++ spec/controllers/admin_categories_controller_spec.rb
@@ -2,12 +2,77 @@ require 'spec_helper'
 
 describe Admin::CategoriesController do
 
-  describe 'create category' do
-    context 'admin wants to create a new category' do
-      it 'creates a new category' do
-        post :create, {category: { name: 'shoes'} }
-        expect(Category.last.name).to eq 'shoes'
+  context "a real admin" do
+
+    before do
+      user = User.create(
+        email: "user@user.com",
+        password: "blah",
+        password_confirmation: "blah",
+        admin: true,
+        first_name: "first",
+        last_name: "last",
+        display_name: "display"
+      )
+
+      login_user(user)
+    end
+
+    describe 'create category' do
+      context 'admin wants to create a new category' do
+        it 'creates a new category' do
+          post :create, {category: { name: 'shoes'} }
+          expect(Category.last.name).to eq 'shoes'
+        end
       end
     end
+  end
+
+  context "non admin user" do
+    before do
+      user = User.create(
+        email: "user@user.com",
+        password: "blah",
+        password_confirmation: "blah",
+        admin: false,
+        first_name: "first",
+        last_name: "last",
+        display_name: "display"
+      )
+
+      login_user(user)
+    end
+    it "cannot go to the admin categories page" do
+      get :index
+      expect(response).to redirect_to login_path
+    end
+
+    it "cannot request a new category object " do
+      get :new
+      expect(response).to redirect_to login_path
+    end
+
+    it "cannot create a new category" do
+      post :create, {category: { name: 'shoes'} }
+      expect(response).to redirect_to login_path
+    end
+
+  end
+  context "not logged in users" do
+    it "cannot go to the admin categories page" do
+      get :index
+      expect(response).to redirect_to login_path
+    end
+
+    it "cannot request a new category object " do
+      get :new
+      expect(response).to redirect_to login_path
+    end
+
+    it "cannot create a new category" do
+      post :create, {category: { name: 'shoes'} }
+      expect(response).to redirect_to login_path
+    end
+
   end
 end
